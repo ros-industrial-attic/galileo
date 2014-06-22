f3544156aab3b5fd462dc1361cd124bce2be54db
Steve Ataucuri Cruz <steve@steve-HP-240-G2-Notebook-PC.(none)>
2014-06-22 18:38:13 -0500

    Add join angle calculation between two vectors  * Modified CMakefiles  * Calculation of join angle between three joints  * Added a function to detect a Pose  * Added messages in order to publish  * Deleted support for frames

--- src/listener_transform.cpp
+++ src/listener_transform.cpp
@@ -1,25 +1,79 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
 //#include <galileo/Skeleton.h>
 #include <vector>
 
+#include <XnOpenNI.h>
+#include <XnCodecIDs.h>
+#include <XnCppWrapper.h>
 
 #define pi 3.141592653589793 
 
 using std::string;
 
-void getTransform(tf::StampedTransform *transform, 
+void getTransform(tf::StampedTransform transform, 
                   tf::TransformListener *listener, 
                   const std::string &from,
                   const std::string &to){
    
-    try{
-        (*listener).lookupTransform(from, to, ros::Time(0), (*transform));
-    }
-    catch (tf::TransformException ex){
+  try
+  {
+	  
+    ros::Time now = ros::Time::now();
+    (*listener).waitForTransform(from, to, ros::Time(0), ros::Duration(3.0));
+    (*listener).lookupTransform(from, to, ros::Time(0), transform);
+    //(*listener).lookupTransform(from, to, ros::Time(0), transform);
+  }
+    /*catch (tf::TransformException ex){
         ROS_ERROR("%s",ex.what());
-    }
+    }*/
+  catch (tf::ExtrapolationException ex)
+  {
+        ROS_ERROR("%s",ex.what());
+  }
+    
+
+}
+
+void print(tf::Matrix3x3 m){
+  ROS_DEBUG("RotM:\n%.5f, %.5f, %.5f\n%.5f, %.5f, %.5f\n%.5f, %.5f, %.5f\n",
+            m[0][0], m[0][1], m[0][2],
+            m[1][0], m[1][1], m[1][2],
+            m[2][0], m[2][1], m[2][2]);
+}
+
+void print(tf::Quaternion q){
+  ROS_DEBUG("Quat: %.4f, %.4f, %.4f, %.4f", q.getX(), q.getY(), q.getZ(), q.w());
+}
+
+void print(tf::Vector3 v){
+  ROS_DEBUG("Vec: %.4f, %.4f, %.4f, %.4f", v.getX(), v.getY(), v.getZ(), v.w());
+}
+
+std::string stream(tf::Vector3 v){
+  std::stringstream ss;
+  ss << v.x() << " "  << v.y() << " "  << v.z();
+  return ss.str();
+}
+
+std::string stream(tf::Quaternion v){
+  std::stringstream ss;
+  ss << v.x() << " "  << v.y() << " "  << v.z()<< " "  << v.w();
+  return ss.str();
+}
+
+std::string stream(tf::StampedTransform tf){
+  std::stringstream ss;
+  ss << stream(tf.getOrigin()) << " " 
+     << stream(tf.getRotation()) << " "
+     << tf.frame_id_ << " " << tf.child_frame_id_;
+  return ss.str();
+}
+
+float getMagnitude(XnVector3D v){
+	return sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
 }
 
 int main(int argc, char** argv)
@@ -29,63 +83,42 @@ int main(int argc, char** argv)
 	ros::NodeHandle node;
 	ros::Publisher skeleton_pub = node.advertise<geometry_msgs::Twist>("skeleton",10);
 
-	tf::TransformListener listener;
+	tf::TransformListener *listener;
+	tf::Vector3 vec;
 
-	std::vector<std::string> frames;
-
-	frames.push_back("head");
-	frames.push_back("neck");
-	frames.push_back("torso");
-
-	frames.push_back("left_shoulder");
-	frames.push_back("left_elbow");
-	frames.push_back("left_hand");
-
-	frames.push_back("right_shoulder");
-	frames.push_back("right_elbow");
-	frames.push_back("right_hand");
-	
 	ros::Rate rate(10.0);
 
 	double elbowAngle = 0.0;
-
+	
+	ros::Time timestamp = ros::Time::now();
 
 	while(node.ok())
-	{
-		tf::StampedTransform transform;
+  {
+	  tf::StampedTransform transform;
 		tf::StampedTransform rShoulderToRElbow;
-        tf::StampedTransform rElbowToRHand;
+    tf::StampedTransform rElbowToRHand;
+		
+    //geometry_msgs::Twist velocity;
+    try
+    {
+      //getTransform(rShoulderToRElbow, listener, "/right_shoulder_2", "/right_elbow_2");
+      //getTransform(&rElbowToRHand, listener, "/right_elbow_2", "/right_hand_2");
+      //listener.lookupTransform("right_shoulder_2", "right_elbow_2", ros::Time(0), rShoulderToRElbow);
+	    (*listener).lookupTransform("/right_elbow_2", "/right_hand_2", ros::Time(0), rElbowToRHand);
 
-        geometry_msgs::Twist velocity;
+     	printf("(%f, %f, %f)",  
+              rElbowToRHand.getOrigin()[0],
+						  rElbowToRHand.getOrigin()[1],
+						  rElbowToRHand.getOrigin()[2]);
+			
+    }
+    catch (tf::TransformException ex)
+    {
+	    ROS_ERROR("... %s",ex.what());
+    }
+    
+    rate.sleep();
 
-        getTransform(&rShoulderToRElbow, &listener, "/right_shoulder", "/right_elbow");
-        getTransform(&rElbowToRHand, &listener, "/right_elbow", "/right_hand");
-
-        //we use the angle between two vectors
-        elbowAngle = (rShoulderToRElbow.getOrigin().angle(rElbowToRHand.getOrigin()));
-        velocity.angular.z = atan2(transform.getOrigin().y(), transform.getOrigin().x());
-		velocity.linear.x = sqrt(pow(transform.getOrigin().x(), 2) + pow(transform.getOrigin().y(), 2));
-
-		printf("angle:%f   angular:%f   linear:%f",elbowAngle, velocity.angular.z, velocity.linear.x);
-		//skeleton_pub.name.push_back()
-
-		/*
-		// we can get from fixed frame
-		for(int i=0; i< frames.size();i++)
-		{
-
-			try
-			{
-				listener.lookupTransform("/openni_depth_frame", frames[i], ros::Time(0), transform);
-				
-			}catch(tf::TransformException e){
-				ROS_ERROR("%s", e.what());
-
-			}
-		} */
-
-		rate.sleep();
-
-	}	
-	return 0;
-}
+	  return 0;
+  }
+} 
--- src/skeleton_angles.cpp
+++ src/skeleton_angles.cpp
@@ -1,5 +1,5 @@
 // this code is based on 
-// openni_tracker.cpp
+// openni_tracker.cpp 
 
 #include <ros/ros.h>
 #include <ros/package.h>
@@ -9,6 +9,13 @@
 #include <XnOpenNI.h>
 #include <XnCodecIDs.h>
 #include <XnCppWrapper.h>
+
+
+#include <galileo/Skeletons.h>
+//#include <galileo/SkeletonJoint.h>
+
+
+#define pi 3.141592653589793
 
 using std::string;
 
@@ -56,76 +63,283 @@ void XN_CALLBACK_TYPE UserPose_PoseDetected(xn::PoseDetectionCapability& capabil
     g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
 }
 
-void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, string const& frame_id, string const& child_frame_id) {
-    static tf::TransformBroadcaster br;
+class UserTracker
+{
+  private:
+    ros::NodeHandle nh;
+    tf::Transformer transformer;
+    ros::Time timestamp;
+    string frame_id;
+    ros::Publisher skeleton_pub;
+  public:
+    /*
+    void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, string const& child_frame_id);
+    //void getMarker(geometry_msgs::Point32* point, std::string const& frame_id);
+    XnPoint3D getBodyLocalization(XnUserID const& user, XnSkeletonJoint const& join);
+    void publishUser(XnUserID const& user);
+    void publishUsers();
+    float getMagnitude(KDL::Vector v);
+    float getLimbAngle(XnUserID const& user, XnSkeletonJoint const& joint1, 
+        XnSkeletonJoint const& joint2, XnSkeletonJoint const& joint3);
+    void checkUserPose(XnUserID const& userid);
+    void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, 
+                      string const& frame_id, string const& child_frame_id);
+    void publishTransforms(const std::string& frame_id);
+    */
+    UserTracker()
+    {
+    
+    }
+  
+    float getMagnitude(KDL::Vector v){
+	    //return sqrt(v.x() * v.X + v.Y * v.Y + v.Z * v.Z);
+      return dot(v,v);
+    }
 
-    XnSkeletonJointPosition joint_position;
-    g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
-    double x = -joint_position.position.X / 1000.0;
-    double y = joint_position.position.Y / 1000.0;
-    double z = joint_position.position.Z / 1000.0;
+    // this function return the localization of a specific joint
+    XnPoint3D getBodyLocalization(XnUserID const& user, XnSkeletonJoint const& joint)
+    {
+      XnPoint3D pt;
 
-    XnSkeletonJointOrientation joint_orientation;
-    g_UserGenerator.GetSkeletonCap().GetSkeletonJointOrientation(user, joint, joint_orientation);
+      if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
+      {
+        printf("User not tracked!\n");
+        pt.X = 0.0;
+        pt.Y = 0.0;
+        pt.Z = 0.0;
+          
+        return pt;
+      }
 
-    XnFloat* m = joint_orientation.orientation.elements;
-    KDL::Rotation rotation(m[0], m[1], m[2],
-    					   m[3], m[4], m[5],
-    					   m[6], m[7], m[8]);
-    double qx, qy, qz, qw;
-    rotation.GetQuaternion(qx, qy, qz, qw);
+      XnSkeletonJointPosition joint_position;
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
 
-    char child_frame_no[128];
-    snprintf(child_frame_no, sizeof(child_frame_no), "%s_%d", child_frame_id.c_str(), user);
+      // Make sure that all joints are found with enough confidence...
+      if (joint_position.fConfidence < 0.7)
+      {
+        pt.X = 0.0;
+        pt.Y = 0.0;
+        pt.Z = 0.0;
+        
+        return pt;
+      }
 
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(x, y, z));
-    transform.setRotation(tf::Quaternion(qx, -qy, -qz, qw));
+      pt = joint_position.position; 
+      return pt;
+    }
 
-    // #4994
-    tf::Transform change_frame;
-    change_frame.setOrigin(tf::Vector3(0, 0, 0));
-    tf::Quaternion frame_rotation;
-    frame_rotation.setEulerZYX(1.5708, 0, 1.5708);
-    change_frame.setRotation(frame_rotation);
+    // We calculate join angle between three joins
+    float getLimbAngle(XnUserID const& user, 
+      XnSkeletonJoint const& joint1, 
+      XnSkeletonJoint const& joint2,
+      XnSkeletonJoint const& joint3)
+    {
+      if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
+      {
+          printf("User not tracked!\n");
+          return -998.0; 
+      }
 
-    transform = change_frame * transform;
+      XnSkeletonJointPosition joint_position1, joint_position2, joint_position3;
+      
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint1, joint_position1);
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint2, joint_position2);
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint3, joint_position3);
 
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_no));
-}
+      if (joint_position1.fConfidence >= 0.9 &&
+          joint_position2.fConfidence >= 0.9)
+      {
+        // We get position 
+        KDL::Vector s(joint_position1.position.X,
+                      joint_position1.position.Y,
+                      joint_position1.position.Z);
+            
+        KDL::Vector e(joint_position2.position.X,
+                      joint_position2.position.Y,
+                      joint_position2.position.Z);
+          
+        KDL::Vector h(joint_position3.position.X,
+                      joint_position3.position.Y,
+                      joint_position3.position.Z);
+      
+        // We calculate two vectors
+        // S: Shoulder, E: Elbow, H: Hand
+        // S  *
+        //    |
+        // E  *---*
+        //        H
+      
+        KDL::Vector v1, v2;
+        // EH = H-E
+        v1 = h-e;
+        v2 = s-e;
 
+        // Calculate the magnitudes of the vectors
+        float v1_magnitude = sqrt(getMagnitude(v1));
+        float v2_magnitude = sqrt(getMagnitude(v2));
+
+
+        if (v1_magnitude != 0.0 && v2_magnitude != 0.0)
+        {
+          // calculate join angle between limps
+          float theta = acos((dot(v1,v2))/(v1_magnitude*v2_magnitude));
+          return theta * 180 / pi;
+        }
+        else
+          return -999.0;
+      }
+      else
+        return -997.0;
+    } 
+
+    // We don't need all limps, just need some important angles of limps
+    void checkUserPose(XnUserID const& userid)
+    {  
+      if (!g_UserGenerator.GetSkeletonCap().IsTracking(userid))
+            return;
+      
+      // Right elbow angle
+      float right_elbow_angle = getLimbAngle(userid, XN_SKEL_RIGHT_SHOULDER, XN_SKEL_RIGHT_ELBOW,
+        XN_SKEL_RIGHT_HAND) * pi / 180.0;
+      printf("Right elbow angle %f\n", right_elbow_angle);
+
+      // Left elbow angle
+      float left_elbow_angle = getLimbAngle(userid, XN_SKEL_LEFT_SHOULDER, 
+        XN_SKEL_LEFT_ELBOW, XN_SKEL_LEFT_HAND) * pi / 180.0;
+      printf("Left elbow angle: %f\n", left_elbow_angle);
+
+      // right shoulder angle
+      float right_shoulder_angle = getLimbAngle(userid, XN_SKEL_RIGHT_HIP, 
+        XN_SKEL_RIGHT_SHOULDER, XN_SKEL_RIGHT_ELBOW) * pi / 180.0;
+      printf("Right Shoulder angle %f\n", right_shoulder_angle);
+
+      // Left shoulder angle
+      float left_shoulder_angle = getLimbAngle(userid, XN_SKEL_LEFT_HIP, 
+        XN_SKEL_LEFT_SHOULDER, XN_SKEL_LEFT_ELBOW) * pi / 180.0;
+      printf("Left Shoulder angle %f\n", left_shoulder_angle);
+
+    }
+
+    void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, 
+                          string const& frame_id, string const& child_frame_id)
+    {
+        static tf::TransformBroadcaster br;
+
+        XnSkeletonJointPosition joint_position;
+        g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
+
+        double x = -joint_position.position.X / 1000.0;
+        double y = joint_position.position.Y / 1000.0;
+        double z = joint_position.position.Z / 1000.0;
+
+        XnSkeletonJointOrientation joint_orientation;
+        g_UserGenerator.GetSkeletonCap().GetSkeletonJointOrientation(user, joint, joint_orientation);
+
+        XnFloat* m = joint_orientation.orientation.elements;
+        KDL::Rotation rotation(m[0], m[1], m[2],
+        					   m[3], m[4], m[5],
+        					   m[6], m[7], m[8]);
+        double qx, qy, qz, qw;
+        rotation.GetQuaternion(qx, qy, qz, qw);
+
+        char child_frame_no[128];
+        snprintf(child_frame_no, sizeof(child_frame_no), "%s_%d", child_frame_id.c_str(), user);
+
+        tf::Transform transform;
+        transform.setOrigin(tf::Vector3(x, y, z));
+        transform.setRotation(tf::Quaternion(qx, -qy, -qz, qw));
+
+        // #4994
+        tf::Transform change_frame;
+        change_frame.setOrigin(tf::Vector3(0, 0, 0));
+        tf::Quaternion frame_rotation;
+        frame_rotation.setEulerZYX(1.5708, 0, 1.5708);
+        change_frame.setRotation(frame_rotation);
+
+        transform = change_frame * transform;
+      
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_no));
+        //transformer.setTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_no));
+
+        
+    }
+
+    void publishTransforms(const std::string& frame_id) {
+        XnUserID users[15];
+        XnUInt16 users_count = 15;
+        g_UserGenerator.GetUsers(users, users_count);
+        
+               
+        for (int i = 0; i < users_count; ++i) {
+            XnUserID user = users[i];
+            if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
+                continue;
+
+
+            publishTransform(user, XN_SKEL_HEAD, frame_id, "head");
+            publishTransform(user, XN_SKEL_NECK, frame_id, "neck");
+            publishTransform(user, XN_SKEL_TORSO, frame_id, "torso");
+
+            publishTransform(user, XN_SKEL_LEFT_SHOULDER, frame_id, "left_shoulder");
+            publishTransform(user, XN_SKEL_LEFT_ELBOW, frame_id, "left_elbow");
+            publishTransform(user, XN_SKEL_LEFT_HAND, frame_id, "left_hand");
+
+            publishTransform(user, XN_SKEL_RIGHT_SHOULDER, frame_id, "right_shoulder");
+            publishTransform(user, XN_SKEL_RIGHT_ELBOW, frame_id, "right_elbow");
+            publishTransform(user, XN_SKEL_RIGHT_HAND, frame_id, "right_hand");
+
+            publishTransform(user, XN_SKEL_LEFT_HIP, frame_id, "left_hip");
+            publishTransform(user, XN_SKEL_LEFT_KNEE, frame_id, "left_knee");
+            publishTransform(user, XN_SKEL_LEFT_FOOT, frame_id, "left_foot");
+
+            publishTransform(user, XN_SKEL_RIGHT_HIP, frame_id, "right_hip");
+            publishTransform(user, XN_SKEL_RIGHT_KNEE, frame_id, "right_knee");
+            publishTransform(user, XN_SKEL_RIGHT_FOOT, frame_id, "right_foot");
+          
+            checkUserPose(user);           
+        }
+    }
+
+};
+/*
 void publishTransforms(const std::string& frame_id) {
     XnUserID users[15];
     XnUInt16 users_count = 15;
     g_UserGenerator.GetUsers(users, users_count);
+    
+    Skeletons skeletons;
 
     for (int i = 0; i < users_count; ++i) {
         XnUserID user = users[i];
         if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
             continue;
+        
+        Skeleton skeleton;      
+        skeleton.userid = user
+        publishTransform(user, XN_SKEL_HEAD,           frame_id, "neck", skeleton.head);
+        publishTransform(user, XN_SKEL_NECK,           frame_id, "neck", skeleton.neck);
+        publishTransform(user, XN_SKEL_TORSO,          frame_id, "torso", skeleton.torso);
 
+        publishTransform(user, XN_SKEL_LEFT_SHOULDER,  frame_id, "left_shoulder", skeleton.left_shoulder);
+        publishTransform(user, XN_SKEL_LEFT_ELBOW,     frame_id, "left_elbow", skeleton_left_elbow);
+        publishTransform(user, XN_SKEL_LEFT_HAND,      frame_id, "left_hand", skeleton.left_hand);
 
-        publishTransform(user, XN_SKEL_HEAD,           frame_id, "head");
-        publishTransform(user, XN_SKEL_NECK,           frame_id, "neck");
-        publishTransform(user, XN_SKEL_TORSO,          frame_id, "torso");
+        publishTransform(user, XN_SKEL_RIGHT_SHOULDER, frame_id, "right_shoulder", skeleton.right_shoulder);
+        publishTransform(user, XN_SKEL_RIGHT_ELBOW,    frame_id, "right_elbow", skeleton.right_elbow);
+        publishTransform(user, XN_SKEL_RIGHT_HAND,     frame_id, "right_hand", skeleton.right_hand);
 
-        publishTransform(user, XN_SKEL_LEFT_SHOULDER,  frame_id, "left_shoulder");
-        publishTransform(user, XN_SKEL_LEFT_ELBOW,     frame_id, "left_elbow");
-        publishTransform(user, XN_SKEL_LEFT_HAND,      frame_id, "left_hand");
+        publishTransform(user, XN_SKEL_LEFT_HIP,       frame_id, "left_hip", skeleton.left_hip);
+        publishTransform(user, XN_SKEL_LEFT_KNEE,      frame_id, "left_knee", skeleton.left_knee);
+        publishTransform(user, XN_SKEL_LEFT_FOOT,      frame_id, "left_foot", skeleton.left_foot);
 
-        publishTransform(user, XN_SKEL_RIGHT_SHOULDER, frame_id, "right_shoulder");
-        publishTransform(user, XN_SKEL_RIGHT_ELBOW,    frame_id, "right_elbow");
-        publishTransform(user, XN_SKEL_RIGHT_HAND,     frame_id, "right_hand");
+        publishTransform(user, XN_SKEL_RIGHT_HIP,      frame_id, "right_hip", skeleton.right_hip);
+        publishTransform(user, XN_SKEL_RIGHT_KNEE,     frame_id, "right_knee", skeleton.right_knee);
+        publishTransform(user, XN_SKEL_RIGHT_FOOT,     frame_id, "right_foot", skeleton.right_foot);
 
-        publishTransform(user, XN_SKEL_LEFT_HIP,       frame_id, "left_hip");
-        publishTransform(user, XN_SKEL_LEFT_KNEE,      frame_id, "left_knee");
-        publishTransform(user, XN_SKEL_LEFT_FOOT,      frame_id, "left_foot");
-
-        publishTransform(user, XN_SKEL_RIGHT_HIP,      frame_id, "right_hip");
-        publishTransform(user, XN_SKEL_RIGHT_KNEE,     frame_id, "right_knee");
-        publishTransform(user, XN_SKEL_RIGHT_FOOT,     frame_id, "right_foot");
+        skls.skeletons.push_back(skeleton);
     }
 }
+*/
 
 #define CHECK_RC(nRetVal, what)										\
 	if (nRetVal != XN_STATUS_OK)									\
@@ -134,36 +348,38 @@ void publishTransforms(const std::string& frame_id) {
 		return nRetVal;												\
 	}
 
+
+
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "openni_tracker");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "galileo");
+  ros::NodeHandle nh;
 
-    string configFilename = ros::package::getPath("openni_tracker") + "/openni_tracker.xml";
-    XnStatus nRetVal = g_Context.InitFromXmlFile(configFilename.c_str());
-    CHECK_RC(nRetVal, "InitFromXml");
+  string configFilename = ros::package::getPath("openni_tracker") + "/openni_tracker.xml";
+  XnStatus nRetVal = g_Context.InitFromXmlFile(configFilename.c_str());
+  CHECK_RC(nRetVal, "InitFromXml");
 
-    nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
-    CHECK_RC(nRetVal, "Find depth generator");
+  nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
+  CHECK_RC(nRetVal, "Find depth generator");
 
-	nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
-	if (nRetVal != XN_STATUS_OK) {
-		nRetVal = g_UserGenerator.Create(g_Context);
-		CHECK_RC(nRetVal, "Find user generator");
-	}
+  nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
+  if (nRetVal != XN_STATUS_OK) {
+	  nRetVal = g_UserGenerator.Create(g_Context);
+	  CHECK_RC(nRetVal, "Find user generator");
+  }
 
-	if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON)) {
-		ROS_INFO("Supplied user generator doesn't support skeleton");
-		return 1;
-	}
+  if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON)) {
+	  ROS_INFO("Supplied user generator doesn't support skeleton");
+	  return 1;
+  }
 
-    XnCallbackHandle hUserCallbacks;
+  XnCallbackHandle hUserCallbacks;
 	g_UserGenerator.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);
 
 	XnCallbackHandle hCalibrationCallbacks;
 	g_UserGenerator.GetSkeletonCap().RegisterCalibrationCallbacks(UserCalibration_CalibrationStart, UserCalibration_CalibrationEnd, NULL, hCalibrationCallbacks);
 
 	if (g_UserGenerator.GetSkeletonCap().NeedPoseForCalibration()) {
-		g_bNeedPose = TRUE;
+	  g_bNeedPose = TRUE;
 		if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION)) {
 			ROS_INFO("Pose required, but not supported");
 			return 1;
@@ -181,15 +397,18 @@ int main(int argc, char **argv) {
 	CHECK_RC(nRetVal, "StartGenerating");
 
 	ros::Rate r(30);
-
         
-        ros::NodeHandle pnh("~");
-        string frame_id("openni_depth_frame");
-        pnh.getParam("camera_frame_id", frame_id);
-                
+  ros::NodeHandle pnh("~");
+  string frame_id("openni_depth_frame");
+  pnh.getParam("camera_frame_id", frame_id);
+  
+  UserTracker tracker;
+        
 	while (ros::ok()) {
 		g_Context.WaitAndUpdateAll();
-		publishTransforms(frame_id);
+		
+    tracker.publishTransforms(frame_id);
+
 		r.sleep();
 	}
 
