d69ae4d0d6ac4450bc53916aaf43c28574d71c5b
Steve Ataucuri <lord.ataucuri@ucsp.edu.pe>
2014-07-29 21:38:41 -0500

    Add Planar Segmentation of a Surface and Others  * Improve the segmentation of a planar surface with RANSAC algorithm  * Improve filtering of cloud points  * Add rosbag record to launch file  * Add rostopic echo to launch file  * Removed dependency of openni_launch node

--- automatic_painting/src/extract_features_node.cpp
+++ automatic_painting/src/extract_features_node.cpp
@@ -130,10 +130,10 @@ class UserTracker
       max_pitch = 10.0;
       resolution = 1.0;
       
-      sub = nh.subscribe<PointCloud>("points", 1, &UserTracker::extractFeatures, this);
+      sub = nh.subscribe<PointCloud>("surface", 1, &UserTracker::extractFeatures, this);
+      //sub = nh.subscribe<sensor_msgs::PointCloud2>("surface", 1, &UserTracker::extractFeatures, this);
 
       features_pub = nh.advertise<automatic_painting::Features>("features", 1);
-      //skeleton_pub = nh.advertise<galileo::Skeletons>("skeleton", 1);
 
     }
   
@@ -261,7 +261,7 @@ class UserTracker
 
     float getDistance(XnPoint3D point1, XnPoint3D point2)
     {
-          return  sqrt(pow((point2.X - point1.X),2) + 
+      return  sqrt(pow((point2.X - point1.X),2) + 
           pow((point2.Y - point1.Y),2) + 
           pow((point2.Z - point1.Z),2));
     }
@@ -281,9 +281,9 @@ class UserTracker
     
     int getClass(float xpitch, float xyaw)  
     {
-       int label = 0;
-       for(float yaw=min_yaw; yaw <= max_yaw; yaw = yaw + resolution)
-       {
+      int label = 0;
+      for(float yaw=min_yaw; yaw <= max_yaw; yaw = yaw + resolution)
+      {
         for(float pitch=min_pitch; pitch <= max_pitch; pitch = pitch + resolution)
         { //cout<<"[pitch="<<pitch<<" , yaw="<<yaw<<"]"<< " ( " << xpitch <<","<< xyaw<<")"<<endl;
           if(pitch==xpitch && yaw==xyaw) 
@@ -292,7 +292,7 @@ class UserTracker
             label++;         
         }
         label++;
-       }
+      }
       return -1;
     }
     
@@ -305,108 +305,108 @@ class UserTracker
                           string const& frame_id, string const& child_frame_id,
                           automatic_painting::SkeletonJoint &skeletonJoint)
     {
-        static tf::TransformBroadcaster br; 
+      static tf::TransformBroadcaster br; 
 
-        XnSkeletonJointPosition joint_position;
-        g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
-        double x = -joint_position.position.X/1000;
-        double y = joint_position.position.Y/1000;
-        double z = joint_position.position.Z/1000;
-     
-        XnSkeletonJointOrientation joint_orientation;
-        g_UserGenerator.GetSkeletonCap().GetSkeletonJointOrientation(user, joint, joint_orientation);
+      XnSkeletonJointPosition joint_position;
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
+      double x = -joint_position.position.X/1000;
+      double y = joint_position.position.Y/1000;
+      double z = joint_position.position.Z/1000;
+   
+      XnSkeletonJointOrientation joint_orientation;
+      g_UserGenerator.GetSkeletonCap().GetSkeletonJointOrientation(user, joint, joint_orientation);
 
-        XnFloat* m = joint_orientation.orientation.elements;
-        KDL::Rotation rotation(m[0], m[1], m[2],
-        					   m[3], m[4], m[5],
-        					   m[6], m[7], m[8]);
-        //get the quaternion of this matrix
-        double qx, qy, qz, qw;
-        rotation.GetQuaternion(qx, qy, qz, qw);
-        //printf("m[] %f %f %f %f %f %f %f %f %f\n",m[0], m[1], m[2],m[3], m[4], m[5],m[6], m[7], m[8]);
-        //printf("q[] %f %f %f %f\n",qx, qy, qz, qw) ;
+      XnFloat* m = joint_orientation.orientation.elements;
+      KDL::Rotation rotation(m[0], m[1], m[2],
+      					   m[3], m[4], m[5],
+      					   m[6], m[7], m[8]);
+      //get the quaternion of this matrix
+      double qx, qy, qz, qw;
+      rotation.GetQuaternion(qx, qy, qz, qw);
+      //printf("m[] %f %f %f %f %f %f %f %f %f\n",m[0], m[1], m[2],m[3], m[4], m[5],m[6], m[7], m[8]);
+      //printf("q[] %f %f %f %f\n",qx, qy, qz, qw) ;
 
-        tf::Transform transform;  
-        transform.setOrigin(tf::Vector3(x, y, z));
-        transform.setRotation(tf::Quaternion(qx, -qy, -qz, qw));
-    
-        tf::Transform change_frame;
-        change_frame.setOrigin(tf::Vector3(0, 0, 0));
-        tf::Quaternion frame_rotation;
-        frame_rotation.setEulerZYX(HALFPI, 0, HALFPI);
-        change_frame.setRotation(frame_rotation);
-
-        transform = change_frame * transform;
-        
-        geometry_msgs::Vector3 position;
-		    geometry_msgs::Quaternion orientation;
-
-        position.x = x;
-        position.y = y;
-        position.z = z;
-
-        orientation.x = qx;
-        orientation.y = qy;
-        orientation.z = qz;
-        orientation.w = qw;
-
-        skeletonJoint.name = child_frame_id;
-		    skeletonJoint.position = position;
-		    skeletonJoint.orientation = orientation;
-		    
+      tf::Transform transform;  
+      transform.setOrigin(tf::Vector3(x, y, z));
+      transform.setRotation(tf::Quaternion(qx, -qy, -qz, qw));
   
-        double roll, pitch, yaw, XX, YY, ZZ;
-        tf::Quaternion q_orig(qx, qy, -qz, qw);
-        tf::Quaternion q_fix(0.70710678, 0., 0., 0.70710678);
+      tf::Transform change_frame;
+      change_frame.setOrigin(tf::Vector3(0, 0, 0));
+      tf::Quaternion frame_rotation;
+      frame_rotation.setEulerZYX(HALFPI, 0, HALFPI);
+      change_frame.setRotation(frame_rotation);
 
-        tf::Quaternion q_rot =  q_fix * q_orig * q_fix.inverse();
-        /*
-        tf::Quaternion q(transform.getRotation().x(), transform.getRotation().y(), 
-                         transform.getRotation().z(), transform.getRotation().w());*/
+      transform = change_frame * transform;
+      
+      geometry_msgs::Vector3 position;
+	    geometry_msgs::Quaternion orientation;
 
-        geometry_msgs::TransformStamped t;
-        //tf::Quaternion q(qx, qy, qz, qw);        
+      position.x = x;
+      position.y = y;
+      position.z = z;
 
-        t.transform.translation.x = x;
-        t.transform.translation.y = y;
-        t.transform.translation.z = z;
-        t.transform.rotation.x = q_rot.x();
-        t.transform.rotation.y = q_rot.y();
-        t.transform.rotation.z = q_rot.z();
-        t.transform.rotation.w = q_rot.w();
+      orientation.x = qx;
+      orientation.y = qy;
+      orientation.z = qz;
+      orientation.w = qw;
 
-        t.header.stamp = ros::Time::now();
-        t.header.frame_id = frame_id;
-        t.child_frame_id = child_frame_id;
-        
-        
-        tf::Quaternion q(q_rot.x(), q_rot.y(), -q_rot.z(), q_rot.w());
-        tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
- 
-        XX  =  m[6];
-        YY =  m[7];
-        ZZ = m[8];
+      skeletonJoint.name = child_frame_id;
+	    skeletonJoint.position = position;
+	    skeletonJoint.orientation = orientation;
+	    
 
-        x_t  = XX*alpha + (x_t * (1 - alpha));
-        y_t  = YY*alpha + (y_t * (1 - alpha));
-        z_t  = ZZ*alpha + (z_t * (1 - alpha));
-        
-        skeletonJoint.pitch = pitch * 180 / PI;
-        skeletonJoint.yaw = yaw * 180 / PI;
-        //skeletonJoint.pitch2 = (atan2(m[6],sqrt(m[7]*m[7]+m[8]*m[8]))*180.0)/PI;
-        //skeletonJoint.pitch2 = (atan2(x_t,sqrt(y_t*y_t + z_t*z_t))*180.0)/PI;
-        
-        //skeletonJoint.yaw2 = m[0]!=0? (atan2(m[3],m[0])*180)/PI : 0;
+      double roll, pitch, yaw, XX, YY, ZZ;
+      tf::Quaternion q_orig(qx, qy, -qz, qw);
+      tf::Quaternion q_fix(0.70710678, 0., 0., 0.70710678);
 
-        //calculate velocities
-        skeletonJoint.velocity.angular.z = 4.0 * atan2(transform.getOrigin().y(),
-                                        transform.getOrigin().x());
-        skeletonJoint.velocity.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
-                                        pow(transform.getOrigin().y(), 2));
-                
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_id));
-        //br.sendTransform(t);
-        return;        
+      tf::Quaternion q_rot =  q_fix * q_orig * q_fix.inverse();
+      /*
+      tf::Quaternion q(transform.getRotation().x(), transform.getRotation().y(), 
+                       transform.getRotation().z(), transform.getRotation().w());*/
+
+      geometry_msgs::TransformStamped t;
+      //tf::Quaternion q(qx, qy, qz, qw);        
+
+      t.transform.translation.x = x;
+      t.transform.translation.y = y;
+      t.transform.translation.z = z;
+      t.transform.rotation.x = q_rot.x();
+      t.transform.rotation.y = q_rot.y();
+      t.transform.rotation.z = q_rot.z();
+      t.transform.rotation.w = q_rot.w();
+
+      t.header.stamp = ros::Time::now();
+      t.header.frame_id = frame_id;
+      t.child_frame_id = child_frame_id;
+      
+      
+      tf::Quaternion q(q_rot.x(), q_rot.y(), -q_rot.z(), q_rot.w());
+      tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+      XX  =  m[6];
+      YY =  m[7];
+      ZZ = m[8];
+
+      x_t  = XX*alpha + (x_t * (1 - alpha));
+      y_t  = YY*alpha + (y_t * (1 - alpha));
+      z_t  = ZZ*alpha + (z_t * (1 - alpha));
+      
+      skeletonJoint.pitch = pitch * 180 / PI;
+      skeletonJoint.yaw = yaw * 180 / PI;
+      //skeletonJoint.pitch2 = (atan2(m[6],sqrt(m[7]*m[7]+m[8]*m[8]))*180.0)/PI;
+      //skeletonJoint.pitch2 = (atan2(x_t,sqrt(y_t*y_t + z_t*z_t))*180.0)/PI;
+      
+      //skeletonJoint.yaw2 = m[0]!=0? (atan2(m[3],m[0])*180)/PI : 0;
+
+      //calculate velocities
+      skeletonJoint.velocity.angular.z = 4.0 * atan2(transform.getOrigin().y(),
+                                      transform.getOrigin().x());
+      skeletonJoint.velocity.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
+                                      pow(transform.getOrigin().y(), 2));
+              
+      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_id));
+      //br.sendTransform(t);
+      return;        
        
     }
 
@@ -426,13 +426,14 @@ class UserTracker
           publishTransform(user, XN_SKEL_TORSO, frame_id, "torso", torsoJoint);
           //publishTransform(user, XN_SKEL_LEFT_SHOULDER,  frame_id, "left_shoulder", rightHandJoint);
           //publishTransform(user, XN_SKEL_RIGHT_ELBOW,     frame_id, "right_elbow", rightHandJoint);
-          publishTransform(user, XN_SKEL_RIGHT_HAND, frame_id, "right_hand", rightHandJoint);
+          publishTransform(user, XN_SKEL_LEFT_ELBOW, frame_id, "left_elbow", rightHandJoint);
         
 	        
       }
   }
 
   void extractFeatures(const PointCloud::ConstPtr& cloud)  
+  //void extractFeatures(const sensor_msgs::PointCloud2::ConstPtr& msg) 
   {
     
     XnUserID users[15];
@@ -441,91 +442,93 @@ class UserTracker
     g_UserGenerator.GetUsers(users, users_count);
     
 		for (int i = 0; i < users_count; ++i) {
-          XnUserID user = users[i];
-          if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
-            continue;                
-        
-          automatic_painting::Skeleton skeleton;
-          automatic_painting::Features features;
+      XnUserID user = users[i];
+      if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
+        continue;                
 
-          pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-          pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
-          pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-          pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+      automatic_painting::Skeleton skeleton;
+      automatic_painting::Features features;
 
-          kdtree.setInputCloud (cloud); // wait const PointCloudConstPtr &cloud or const PointCloudConstPtr &cloud
+      pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+      pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+      pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+      pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
 
-          XnPoint3D hand = getBodyLocalization(user, XN_SKEL_RIGHT_HAND);
-          XnPoint3D torso = getBodyLocalization(user, XN_SKEL_TORSO);
+      //PointCloud::ConstPtr cloud;
+      //pcl::fromROSMsg(*msg, cloud);
+      kdtree.setInputCloud (cloud); // wait const PointCloudConstPtr &cloud or const PointCloudConstPtr &cloud
 
-          pcl::PointXYZ searchPoint; // this is joint position
-          pcl::PointXYZ closestPoint; // the closest point to surface
-          geometry_msgs::Vector3 closestPointm;   
-          geometry_msgs::Vector3 basePoint;
+      XnPoint3D hand = getBodyLocalization(user, XN_SKEL_LEFT_HAND);
+      XnPoint3D torso = getBodyLocalization(user, XN_SKEL_TORSO);
 
-          searchPoint.x = hand.X;          
-          searchPoint.y = hand.Y;
-          searchPoint.z = hand.Z;
+      pcl::PointXYZ searchPoint; // this is joint position
+      pcl::PointXYZ closestPoint; // the closest point to surface
+      geometry_msgs::Vector3 closestPointm;   
+      geometry_msgs::Vector3 basePoint;
 
-          basePoint.x = torso.X;
-          basePoint.y = torso.Y;
-          basePoint.z = torso.Z;
+      searchPoint.x = hand.X;          
+      searchPoint.y = hand.Y;
+      searchPoint.z = hand.Z;
+
+      basePoint.x = torso.X;
+      basePoint.y = torso.Y;
+      basePoint.z = torso.Z;
 
 
-          // K nearest neighbor search
-          vector<int> pointIdxNKNSearch(K);
-          vector<float> pointNKNSquaredDistance(K);
+      // K nearest neighbor search
+      vector<int> pointIdxNKNSearch(K);
+      vector<float> pointNKNSquaredDistance(K);
 
-          std::cout << "K nearest neighbor of hand point (" << searchPoint.x 
-                    << " " << searchPoint.y 
-                    << " " << searchPoint.z
-                    << ") with K=" << K << std::endl;
+      /*std::cout << "K nearest neighbor of hand point (" << searchPoint.x 
+                << " " << searchPoint.y 
+                << " " << searchPoint.z
+                << ") with K=" << K << std::endl;*/
 
-          if ( kdtree.nearestKSearch (searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
-          {
-            for (size_t i = 0; i < pointIdxNKNSearch.size (); ++i)
-            {  cout << " " << cloud->points[ pointIdxNKNSearch[i] ].x 
-                   << " " << cloud->points[ pointIdxNKNSearch[i] ].y 
-                   << " " << cloud->points[ pointIdxNKNSearch[i] ].z 
-                   << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
-               features.distances.push_back(pointNKNSquaredDistance[i]); 
-            }
-          }
-          
-                
-          closestPoint.x = cloud->points[pointIdxNKNSearch[0]].x; 
-          closestPoint.y = cloud->points[pointIdxNKNSearch[0]].y; 
-          closestPoint.z = cloud->points[pointIdxNKNSearch[0]].z; 
-          closestPointm.x = closestPoint.x; 
-          closestPointm.y = closestPoint.y;
-          closestPointm.z = closestPoint.z;
+      if ( kdtree.nearestKSearch (searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
+      {
+        for (size_t i = 0; i < pointIdxNKNSearch.size (); ++i)
+        {  /*cout << " " << cloud->points[ pointIdxNKNSearch[i] ].x 
+               << " " << cloud->points[ pointIdxNKNSearch[i] ].y 
+               << " " << cloud->points[ pointIdxNKNSearch[i] ].z 
+               << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;*/
+           features.distances.push_back(pointNKNSquaredDistance[i]); 
+        }
+      }
 
-          ne.setViewPoint (closestPoint.x, closestPoint.y, closestPoint.z); // normals around the closest point
-          ne.setInputCloud (cloud);
-          ne.setSearchMethod (tree);
+            
+      closestPoint.x = cloud->points[pointIdxNKNSearch[0]].x; 
+      closestPoint.y = cloud->points[pointIdxNKNSearch[0]].y; 
+      closestPoint.z = cloud->points[pointIdxNKNSearch[0]].z; 
+      closestPointm.x = closestPoint.x; 
+      closestPointm.y = closestPoint.y;
+      closestPointm.z = closestPoint.z;
 
-          // Use all neighbors in a sphere of radius 3cm
-          ne.setRadiusSearch (0.03);
+      ne.setViewPoint (closestPoint.x, closestPoint.y, closestPoint.z); // normals around the closest point
+      ne.setInputCloud (cloud);
+      ne.setSearchMethod (tree);
 
-          // Compute the features
-          ne.compute (*normals);
+      // Use all neighbors in a sphere of radius 3cm
+      ne.setRadiusSearch (0.03);
 
-          features.rightHand = rightHandJoint;
-          //features.torsoJoint = torsoJoint;
+      // Compute the features
+      ne.compute (*normals);
 
-          features.closestPoint = closestPointm;
-          features.basePoint = basePoint;
+      features.rightHand = rightHandJoint;
+      //features.torsoJoint = torsoJoint;
 
-          // we publish only if we get angles between param and calculate the class of features
-          int cls = getClass(rightHandJoint.pitch, rightHandJoint.yaw);
-          /*if(label=!-1)
-          {
-            features.label = label;
-            features_pub.publish(features);
-          }*/
-          
-          features.cls = cls; //rand() % 400 + 1;
-          features_pub.publish(features);
+      features.closestPoint = closestPointm;
+      features.basePoint = basePoint;
+
+      // we publish only if we get angles between param and calculate the class of features
+      int cls = getClass(rightHandJoint.pitch, rightHandJoint.yaw);
+      /*if(label=!-1)
+      {
+        features.label = label;
+        features_pub.publish(features);
+      }*/
+
+      features.cls = cls; //rand() % 400 + 1;
+      features_pub.publish(features);
          
           
     } // endfor
--- automatic_painting/src/point_cloud_node.cpp
+++ automatic_painting/src/point_cloud_node.cpp
@@ -5,9 +5,153 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/io/openni_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/openni_camera/openni_driver.h>
+#include <pcl/console/parse.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
 
-ros::Publisher pub;
+
+template <typename PointType>
+class CloudPoints
+{
+  public:
+    typedef pcl::PointCloud<PointType> Cloud;
+    typedef typename Cloud::Ptr CloudPtr;
+    typedef typename Cloud::ConstPtr CloudConstPtr;
+
+    pcl::visualization::CloudViewer viewer;
+    pcl::VoxelGrid<PointType> grid_;
+    pcl::SACSegmentation<PointType> seg_;
+    pcl::ExtractIndices<PointType> extract_;
+
+    boost::mutex mtx_;
+    CloudConstPtr cloud_;    
+
+    ros::NodeHandle nh;
+    ros::Publisher pub;
+
+    CloudPoints( double threshold = 0.01)
+      : viewer ("Planar Segmentation Viewer")
+    {
+      pub = nh.advertise<sensor_msgs::PointCloud2> ("surface", 1);
+
+      grid_.setFilterFieldName ("z");
+      grid_.setFilterLimits (0.0f, 3.0f);
+      grid_.setLeafSize (0.01f, 0.01f, 0.01f);
+
+      seg_.setOptimizeCoefficients (true);
+      seg_.setModelType (pcl::SACMODEL_PLANE);
+      seg_.setMethodType (pcl::SAC_RANSAC);
+      seg_.setMaxIterations (1000);
+      seg_.setDistanceThreshold (threshold);
+
+      extract_.setNegative (false);
+    }
+
+    void 
+    cloud_cb_ (const CloudConstPtr& cloud)
+    {
+      set (cloud);
+    }
+
+    void
+    set (const CloudConstPtr& cloud)
+    {
+      //lock while we set our cloud;
+      boost::mutex::scoped_lock lock (mtx_);
+      cloud_  = cloud;
+    }
+
+    CloudPtr
+    get ()
+    {
+      //lock while we swap our cloud and reset it.
+      boost::mutex::scoped_lock lock (mtx_);
+      CloudPtr cloud_filtered (new Cloud);
+      CloudPtr temp_cloud1 (new Cloud);
+      CloudPtr temp_cloud2 (new Cloud);
+
+      //first apply a filter
+      grid_.setInputCloud (cloud_);
+      grid_.filter (*cloud_filtered);
+
+      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+      pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+
+      seg_.setInputCloud (cloud_filtered);
+      seg_.segment (*inliers, *coefficients);
+
+      extract_.setInputCloud (cloud_filtered);
+      extract_.setIndices (inliers);
+      extract_.filter (*temp_cloud1);
+
+      extract_.setNegative(true);
+      extract_.filter(*temp_cloud2);
+      cloud_filtered.swap(temp_cloud2);
+
+      return (temp_cloud2);
+    }
+
+    void
+    run ()
+    {
+      pcl::Grabber* interface = new pcl::OpenNIGrabber ();
+
+      boost::function<void (const CloudConstPtr&)> f = boost::bind (&CloudPoints::cloud_cb_, this, _1);
+      boost::signals2::connection c = interface->registerCallback (f);
+      
+      interface->start ();
+      
+      while (!viewer.wasStopped ())
+      {
+        if (cloud_)
+        {
+          //the call to get() sets the cloud_ to null;
+          CloudPtr c = get();
+          viewer.showCloud (c);
+          sensor_msgs::PointCloud2 msg;
+          pcl::toROSMsg(*c,msg);
+          pub.publish (msg);
+        }
+        ros::spinOnce();
+      }
+
+      interface->stop ();
+    }
+
+};
+
+
+int main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "cloud_points");
+  
+  //ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
+  double threshold = 0.01;
+
+  pcl::OpenNIGrabber grabber;
+
+  if (grabber.providesCallback<pcl::OpenNIGrabber::sig_cb_openni_point_cloud_rgba> ())
+  {
+    CloudPoints<pcl::PointXYZRGBA> v(threshold);
+    v.run ();
+  }
+  else
+  {
+    CloudPoints<pcl::PointXYZ> v (threshold);
+    v.run ();
+  } 
+ 
+}
+
+/*
+
 
 void 
 cloud_cb (const pcl::PCLPointCloud2ConstPtr& input)
@@ -24,16 +168,4 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& input)
   pub.publish (cloud_filtered);
 }
 
-int main (int argc, char** argv)
-{
-  // Initialize ROS
-  ros::init (argc, argv, "cloud_points");
-  ros::NodeHandle nh;
-
-  ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
-
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("surface", 1);
-  
-  ros::spin ();
-}
-
+*/
